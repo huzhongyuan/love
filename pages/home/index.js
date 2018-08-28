c95ce800ae1a918e5523cc46fc9dc4995eec5080
huzhongyuan <1141422520@qq.com>
2018-08-28 11:03:54 +0800

    细节修改

--- pages/home/index.js
+++ pages/home/index.js
@@ -55,9 +55,9 @@ exports.default = Page({
   //点击图片触发事件
   swipclick: function (e) {
     console.log(this.data.swiperCurrent);
-    wx.switchTab({
-      url: this.data.links[this.data.swiperCurrent]
-    })
+    // wx.switchTab({
+    //   url: this.data.links[this.data.swiperCurrent]
+    // })
   },
   //查看文章列表
   checkmore: function(e) {
